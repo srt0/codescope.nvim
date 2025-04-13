9c3b4a0b0c2988362aa5ab5cb22789052b18eef8
Saikat Ray <saikat.tapu@gmail.com>
2025-04-14 02:54:58 +0600

    added colors directory

---
+++ colors/everblush.vim
@@ -0,0 +1,1 @@
+lua require("everblush").setup()
--- lua/everblush/init.lua
+++ lua/everblush/init.lua
@@ -1,5 +1,4 @@
 -- Copyright (c) 2024 [srt0]. MIT License.
--- GitHub: https://github.com/srt0/everblush.nvim
 
 local M = {}
 local palette = require("everblush.palette")
@@ -9,6 +8,10 @@ M.setup = function(opts)
   opts = opts or {}
   vim.g.everblush_transparent = opts.transparent or false
 
+  -- Set colorscheme name FIRST
+  vim.g.colors_name = "everblush"
+
+  -- Rest of your setup logic
   if vim.g.everblush_transparent then
     palette.colors.bg = "NONE"
   end
@@ -16,16 +19,12 @@ M.setup = function(opts)
   vim.o.termguicolors = true
   vim.o.background = "dark"
 
-  -- Apply highlights
   for group, settings in pairs(highlights.groups) do
     if settings.bg and vim.g.everblush_transparent then
       settings.bg = "NONE"
     end
     vim.api.nvim_set_hl(0, group, settings)
   end
-
-  -- Critical fix: Register the colorscheme
-  vim.g.colors_name = "everblush"
 end
 
 return M
